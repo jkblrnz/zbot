73570c894e867e61ae06efe92062b15f2f6abb2b
Ubuntu <ubuntu@ip-172-31-3-176.us-east-2.compute.internal>
2021-09-13 08:42:31 +0000

    kinda working

--- cogs/lookUp.py
+++ cogs/lookUp.py
@@ -1,34 +1,28 @@
+#download for grequests to fix issues with ssl
+#tends to cause some of the rest events to go into inf loops
+#grequests used in boss list to request pages for boss enum
+from gevent import monkey as curious_george
+curious_george.patch_all(thread=False, select=False)
+import grequests
+import requests
+
+#api stuff
 import discord
 from discord.ext import commands
+#Expirmental wrapper for discord slash commands
+#When offical rewrite is out a rewrite might be nesscary
 import discord_slash
 from discord_slash import cog_ext, SlashContext
 
-from enum import Enum
-from unidecode import unidecode
+#data object stuff
+from aenum import Enum, NoAlias, auto
 import json
 
-guildID = [870765414374854736, 882096026775855184]
-
-# open and load relevant json files
-with open('json/equipmentItem') as json_file:
-    f = open('json/equipmentItem')
-    equipData = json.load(f)
-    f.close
-
-with open('json/commandSkill') as json_file:
-    f = open('json/commandSkill')
-    skillData = json.load(f)
-    f.close
-
-with open('json/combinedPassiveSkill') as json_file:
-    f = open('json/combinedPassiveSkill')
-    combinedPassiveSkillData = json.load(f)
-    f.close
-
-with open('json/passiveSkill') as json_file:
-    f = open('json/passiveSkill')
-    passiveSkillData = json.load(f)
-    f.close
+#search stuff
+import re
+#primarily used in item search for forgien words
+#normalizses unicode to let english users search for the french named items
+from unidecode import unidecode
 
 class WeaponElement(Enum):
     Dark = 6
@@ -69,28 +63,83 @@ class ItemType(Enum):
     Upper_Slot = 5
     Lower_Slot = 6
 
+# this class has partial searches so needs to be managed carefully
+class Boss(Enum):
+    Hildegard = auto()
+    Matilda_And_Hildegard = auto()
+    Matilda_And_Upa_Jellyfish = auto()
+    Hiveval = auto()
+    Grimwasp = auto()
+    Counterattacking_Grimwasp = auto()
+    Dark_Knight = auto()
+    Shadowlurker_Dark_Knight = auto()
+    Mitra_Golem = auto()
+    Scape_Ox = auto()
+    Equismaton = auto()
+    Roaring_Scape_Ox = auto()
+    Graenill = auto()
+    Almudahd = auto()
+    Zarich = auto()
+    Zarich_And_Almudahd = auto()
+    Xenon = auto()
+    Empty_Head = auto()
+    Snowlia = auto()
+    Sky_Pirate_Head_Of_Obsession = auto()
+    Zafosboa = auto()
+    Bone_Python = auto()
+    Triwhisp = auto()
+    Rem_Nawis = auto()
+    Ines = auto()
+
 class lookUp(commands.Cog):
+
+    guildList = []
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+        # load json item data
+        # equipment data
+        with open('json/equipmentItem') as json_file:
+            f = open('json/equipmentItem')
+            self.__equipData = json.load(f)
+            f.close
+
+        # skill data
+        with open('json/commandSkill') as json_file:
+            f = open('json/commandSkill')
+            self.__skillData = json.load(f)
+            f.close
+
+        # synth data
+        with open('json/combinedPassiveSkill') as json_file:
+            f = open('json/combinedPassiveSkill')
+            self.__combinedPassiveSkillData = json.load(f)
+            f.close
+
+        # skill data
+        with open('json/passiveSkill') as json_file:
+            f = open('json/passiveSkill')
+            self.__passiveSkillData = json.load(f)
+            f.close
+
     # REST events
     @commands.Cog.listener("on_ready")
-    async def locations(client):
-        print(f'{client.bot.user} is connected to this guild:')
-        for guild in client.bot.guilds:
+    async def locations(self):
+        print(f'{self.bot.user} is connected to this guild:')
+        for guild in self.bot.guilds:
             print(f'{guild.name}(id: {guild.id})')
+            self.guildList.append(guild.id)
 
     @commands.Cog.listener("on_reaction_add")
     async def remove(message, reaction, user):
         if reaction.message.author.bot and reaction.emoji == "❌":
             await reaction.message.delete()
 
-    @cog_ext.cog_slash(name="item", description="Search for a particular item.", guild_ids = guildID)
+    @cog_ext.cog_slash(name="item", description="Search for a particular item.", guild_ids = guildList)
     async def item(self, ctx: discord_slash.SlashContext, name):
-        if ctx.author == self.bot.user: return
         if ctx.author.bot: return
         messageName = unidecode(name.lower())
-        for items in equipData['BookList']:
+        for items in self.__equipData['BookList']:
             itemName = unidecode(items['name'].lower())
             itemId = str(items['id'])
             if (messageName in itemName or itemId == messageName) or messageName in itemId:
@@ -121,7 +170,7 @@ class lookUp(commands.Cog):
                                 "***Evasion*** : " + str(items['maxStat']['evasionRating']) + " - " + str(items['limitBreakMaxStat']['evasionRating']),
                                 inline = True)
 
-                for skill in skillData['BookList']:
+                for skill in self.__skillData['BookList']:
                     if skill['id'] == items['skillId']:
                         #movement type
                         if skill['rowTypeAfterCast'] == 1:
@@ -157,7 +206,7 @@ class lookUp(commands.Cog):
                                         inline = False)
 
                 #item skills are located in a seperate json object requiring a seprate search
-                for passive in passiveSkillData['BookList']:
+                for passive in self.__passiveSkillData['BookList']:
                     if passive['id'] == items['skillId']:
                         embed.add_field(name = "***Skill***", value =
                                         str(passive['display']) + '\n' +
@@ -166,9 +215,9 @@ class lookUp(commands.Cog):
                         valueString = "​"
                         #synths are in seprate json object and each armor should have one so safe to assume
                         #armorUnit tests this assertion
-                        for combinedPassives in items['combinedPassiveSkillIds']:
-                            for combined in combinedPassiveSkillData['BookList']:
-                                if combined['id'] == combinedPassives:
+                        for self.__combinedPassives in items['combinedPassiveSkillIds']:
+                            for combined in self.__combinedPassiveSkillData['BookList']:
+                                if combined['id'] == self.__combinedPassives:
                                     valueString += "**" + combined['name'] + "**" + '\n' + combined['display'] + '\n'
 
                         embed.add_field(name = "***Synths***", value =
@@ -182,13 +231,12 @@ class lookUp(commands.Cog):
             await ctx.send(response, hidden=True)
             return
 
-    @cog_ext.cog_slash(name="search", description="Search for items with a matching name.", guild_ids = guildID)
+    @cog_ext.cog_slash(name="search", description="Search for items with a matching name.", guild_ids = guildList)
     async def search(self, ctx: discord_slash.SlashContext, name):
-       if ctx.author == self.bot.user: return
        if ctx.author.bot: return
 
        response = "```"
-       for items in equipData['BookList']:
+       for items in self.__equipData['BookList']:
            if name.lower() in unidecode(items['name'].lower()):
                response += items['name'] + ":" + str(items['rarity']) + ":" + str(items['id']) + ", "
        response = response[:-2] + "```"
@@ -196,15 +244,15 @@ class lookUp(commands.Cog):
        if len(response) == 4:
            response = "```No Matches```"
            await ctx.send(response, hidden=True)
-       else:
-           await ctx.send(response, hidden=True)
+           return
+       await ctx.send(response, hidden=True)
 
-    @cog_ext.cog_slash(name="type", description="Search for all items of a ceartin type.  Returns name:rarity:id.", guild_ids = guildID)
+    @cog_ext.cog_slash(name="type", description="Search for all items of a ceartin type.  Returns name:rarity:id.", guild_ids = guildList)
     async def type(self, ctx: discord_slash.SlashContext, item_type):
-        if ctx.author == self.bot.user: return
         if ctx.author.bot: return
+
         response = "```"
-        for items in equipData['BookList']:
+        for items in self.__equipData['BookList']:
             if items['slot'] == 1:
                 itype = WeaponType(items['typeId']).name
             else:
@@ -217,8 +265,51 @@ class lookUp(commands.Cog):
         if len(response) == 4:
             response = "```No Matches```"
             await ctx.send(response, hidden=True)
+            return
+
+        await ctx.send(response, hidden=True)
+
+    @cog_ext.cog_slash(name="boss", description="Search for a boss guide.", guild_ids = guildList)
+    async def boss(self, ctx: discord_slash.SlashContext, boss):
+        if ctx.author.bot: return
+
+        # this solution finds first parital match so Boss enum needs to be carfully managed
+        bossName = ''
+        for bosses in Boss.__members__:
+            if boss.lower() in bosses.lower().replace("_"," "):
+                bossName = bosses
+                print(bosses.name.value[1])
+                break
+
+        if bossName != '':
+             response = requests.get("https://altema-mitrasphere.com/" + bossName.replace("_",""))
+             if "404-img.png" in str(response.content): #altema doesn't return right code so look for their 404 img
+                 await ctx.send("```Boss not found on altema```", hidden=True)
+             else:
+                 await ctx.send("https://altema-mitrasphere.com/" + bossName.replace("_","").replace("And",""))
         else:
-            await ctx.send(response, hidden=True)
+             await ctx.send("```Boss not found in database```", hidden=True)
+
+    @cog_ext.cog_slash(name="bossList", description="Get a list of all possible bosses.", guild_ids = guildList)
+    async def bossList(self, ctx: discord_slash.SlashContext):
+        if ctx.author.bot: return
+
+        urls = []
+        for bosses in Boss.__members__:
+            urls.append("https://altema-mitrasphere.com/" + bosses.replace("_","").replace("And",""))
+        rs = (grequests.get(u) for u in urls)
+        responses = grequests.map(rs)
+
+        lst = ""
+        for x in range(len(responses)):
+            target = re.compile("404-img.png")
+            if target.search(str(responses[x].content)): #altema doesn't return the right code so look for the 404 img
+                lst += Boss(x + 1).name.replace("_"," ") + '\n'
+            else:
+                lst += '[' + Boss(x + 1).name.replace("_", " ") + ']' + "(https://altema-mitrasphere.com/" + Boss(x + 1).name.replace("_","").replace("And","") + ')' + '\n'
+
+        embed = discord.Embed(description=lst)
+        await ctx.send(embed=embed, hidden=True)
 
 def setup(bot):
     bot.add_cog(lookUp(bot))
